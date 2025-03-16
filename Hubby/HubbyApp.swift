2701371c280ee4eeb2a865cecb72bf887c14a727
Sviatoslav Dykyi <sviatoslav.dykyi@audacy.com>
2025-03-16 15:14:18 +0100

    Initial Commit

---
+++ Hubby/HubbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HubbyApp.swift
+//  Hubby
+//
+//  Created by Sviatoslav Dykyi on 16.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct HubbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
